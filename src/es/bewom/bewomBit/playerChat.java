c7bb1fefec12b1af9ae7babcb8fee3f9e420e298
pagoru <pagoru@gmail.com>
2014-07-29 19:58:51 +0200

    permisos chat

--- src/es/bewom/bewomBit/playerChat.java
+++ src/es/bewom/bewomBit/playerChat.java
@@ -1,14 +1,11 @@
 package es.bewom.bewomBit;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.scoreboard.Scoreboard;
-import org.bukkit.scoreboard.ScoreboardManager;
-import org.bukkit.scoreboard.Team;
 
 
 @SuppressWarnings("deprecation")
@@ -25,23 +22,16 @@ public class playerChat implements Listener {
 		String playerName = eventChat.getPlayer().getName(); //limpio String 
 
 		if (craftPlayer.hasPermission("bewom.admin")) {
-			if (colorChange == true) {
-				eventChat.setFormat(">> "+ ChatColor.DARK_RED + "" + ChatColor.BOLD + playerName + ChatColor.RESET + " < " + ChatColor.GRAY + ChatColor.BOLD + message);
-				colorChange = false;
-			} else {
-				eventChat.setFormat(">> "+ ChatColor.DARK_RED + "" + ChatColor.BOLD + playerName + ChatColor.RESET + " < " + ChatColor.WHITE  + ChatColor.BOLD + message);
-				colorChange = true;
-			}
+			eventChat.setFormat(">> "+ ChatColor.DARK_RED + "" + ChatColor.BOLD + playerName + ChatColor.RESET + " < " + ChatColor.WHITE  + ChatColor.BOLD + message);
+			
+		} else if (craftPlayer.hasPermission("bewom.mod")) {
+			eventChat.setFormat(">> "+ ChatColor.DARK_GREEN + "" + ChatColor.BOLD + playerName + ChatColor.RESET + " < " + ChatColor.WHITE  + ChatColor.BOLD + message);
+			
+		} else if (craftPlayer.hasPermission("bewom.vip")) {
+			eventChat.setFormat(">> "+ ChatColor.DARK_AQUA + playerName + " < " + message);
 			
 		} else {
-			
-			if (colorChange == true) {
-				eventChat.setFormat(">> "+ playerName + " < " + ChatColor.GRAY + message);
-				colorChange = false;
-			} else {
-				eventChat.setFormat(">> "+ playerName + " < " + ChatColor.WHITE + message);
-				colorChange = true;
-			}
+			eventChat.setFormat(">> "+ playerName + " < " + ChatColor.GRAY + message);
 			
 		}
 		
