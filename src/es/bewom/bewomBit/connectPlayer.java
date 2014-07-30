e3dc178e79c29933f2e372966242b752a209b357
pagoru <pagoru@gmail.com>
2014-07-30 03:49:53 +0200

    Limpio despues de hacer tests

--- src/es/bewom/bewomBit/connectPlayer.java
+++ src/es/bewom/bewomBit/connectPlayer.java
@@ -1,7 +1,7 @@
 package es.bewom.bewomBit;
 
 import java.sql.SQLException;
-  //ADSFFDSFasdfa
+  
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
