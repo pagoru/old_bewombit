0b286d93bc462b8f76ebfad43071a4db2c64c2af
Carlo <carlopadgom@gmail.com>
2014-08-04 02:05:43 +0100

    Meter el PLayer craftPLayerArgs en el if del kick

--- src/es/bewom/bewomBit/commands/commandKick.java
+++ src/es/bewom/bewomBit/commands/commandKick.java
@@ -7,52 +7,52 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class commandKick {
-	
+
 	@SuppressWarnings("deprecation")
 	public static boolean commandkick(CommandSender sender, Command cmd, String label, String[] args){
-		Player craftPlayerArgs;
-		
-		String broadcast = ChatColor.DARK_BLUE + ": " + ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "WOM" + ChatColor.DARK_BLUE + " < ";
-		
-		
+
+
 		if (label.equalsIgnoreCase("kick")){
+			Player craftPlayerArgs;
+			String broadcast = ChatColor.DARK_BLUE + ": " + ChatColor.DARK_BLUE + "" + ChatColor.BOLD + "WOM" + ChatColor.DARK_BLUE + " < ";
+
 			if (args.length == 1) {
 				if (sender.getServer().getPlayer(args[0]) != null){
-					
+
 					craftPlayerArgs = Bukkit.getServer().getPlayer(args[0]);
 					String playerArgsName = craftPlayerArgs.getName();
-					
+
 					craftPlayerArgs.kickPlayer("");
-					
+
 					Bukkit.getServer().broadcastMessage(broadcast + ChatColor.BLUE + "El jugador " + playerArgsName + " ha sido kickeado.");
-					
+
 				} else {
-					
+
 					sender.sendMessage(ChatColor.RED + "El jugador no esta conectado.");
 					return true;
 				}
-				
+
 			} else if (args.length > 1){
 				if (sender.getServer().getPlayer(args[0]) != null){
-				
+
 					craftPlayerArgs = Bukkit.getServer().getPlayer(args[0]);
 					String playerArgsName = craftPlayerArgs.getName();
-					
+
 					String texto = "";
 					for (int i = 1; i < args.length; i++) {
 						texto += args[i] + " ";
 					}
-					
+
 					craftPlayerArgs.kickPlayer("");
-					
+
 					Bukkit.getServer().broadcastMessage(broadcast + ChatColor.BLUE + "El jugador " + playerArgsName + " ha sido kickeado por " + texto);
 				} else {
-					
+
 					sender.sendMessage(ChatColor.RED + "El jugador no esta conectado.");
 					return true;
 				}
 			} 
-			
+
 		} else {
 			sender.sendMessage(ChatColor.RED + "La forma correcta es /kick <player> [motivo]");
 			return true;
