3b7fdb392943de2c47a8827a8d03cc5d6fdde3cb
Carlo <carlopadgom@gmail.com>
2014-08-05 17:52:47 +0100

    Ahora /kill te dice los niveles que tenía el asesinado.

--- src/es/bewom/bewomBit/commands/commandKill.java
+++ src/es/bewom/bewomBit/commands/commandKill.java
@@ -7,41 +7,52 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class commandKill {
-	
+
 	@SuppressWarnings("deprecation")
 	public static boolean commandkill(CommandSender sender, Command cmd, String label, String [] args){
-		
+
 		if (label.equalsIgnoreCase("kill")){
 			Player craftPlayer = (Player) sender;
 			Player craftPlayerArgs;
-			
+
 			if (args.length == 1) {
-				
+
 				if (sender.getServer().getPlayer(args [0]) != null){
-					
+
 					craftPlayerArgs = Bukkit.getServer().getPlayer(args[0]);
 					int nivel = craftPlayerArgs.getLevel();
 					craftPlayerArgs.setHealth(0);
 					String playerArgsName = craftPlayerArgs.getName();
-					
-					craftPlayer.sendMessage(ChatColor.GRAY + "Has matado a " + playerArgsName + " y le has hecho perder " + nivel + " niveles.");
+
+					switch (nivel){
+
+					case 0:
+						craftPlayer.sendMessage(ChatColor.GRAY + "Has matado a " + playerArgsName + ", no tenía niveles.");
+						break;
+					case 1:
+						craftPlayer.sendMessage(ChatColor.GRAY + "Has matado a " + playerArgsName + ", tenía " + nivel + " nivel.");
+						break;
+					default:
+						craftPlayer.sendMessage(ChatColor.GRAY + "Has matado a " + playerArgsName + ", tenía " + nivel + " niveles.");
+						break;
+					}
 
 					return true;
-					
+
 				} else {
 					craftPlayer.sendMessage(ChatColor.RED + "El jugador no esta conectado.");
 					return true;
 				}
-				
+
 			} else {
 				sender.sendMessage(ChatColor.RED + "La forma correcta es /kill <player>");
 				return true;
 			}
-			
-			
+
+
 		}
 		return false;
 	}
-		
+
 
 }
