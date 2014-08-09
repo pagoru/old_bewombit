04d967169c67060d10bc91099f2af3a5f6455258
Pablo <pagoru@gmail.com>
2014-08-09 02:50:06 +0200

    Carpeta "utility" para los comandos + nuevo paquete para los comandos autocompletables!

--- src/es/bewom/bewomBit/commands/utility/autoCompleteTab.java
+++ src/es/bewom/bewomBit/commands/utility/autoCompleteTab.java
@@ -1,57 +1,24 @@
 package es.bewom.bewomBit.commands.utility;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
-import org.bukkit.entity.Player;
+
+import es.bewom.bewomBit.commands.commandsAutoComplete.commandAutoP;
 
 public class autoCompleteTab implements TabCompleter {
 
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
 		
-		Player craftPlayer = (Player) sender;
-		
-		if(cmd.getName().equalsIgnoreCase("p")){
+		if(commandAutoP.commandautop(sender, cmd, alias, args) != null){
 			
-			if (args.length == 1){
-				List<String> pList = new ArrayList<String>();  
-		         
-				pList.add("private");
-				pList.add("publico");
-				
-				if (craftPlayer.hasPermission("bewom.admin") || craftPlayer.hasPermission("bewom.mod")){
-					pList.add("cambiar");
-				}
-				
-				return pList;
-			} else if(args.length == 2 && args[0].equals("cambiar")){
-				
-				if (craftPlayer.hasPermission("bewom.admin") || craftPlayer.hasPermission("bewom.mod")){
-					List<String> pList = new ArrayList<String>();  
-			        
-					pList.add("propietario");
-					pList.add("estado");
-					
-					return pList;
-				}
-				
-			} else if(args.length == 3 && args[0].equals("cambiar") && args[1].equals("estado")){
+			return commandAutoP.commandautop(sender, cmd, alias, args);
 			
-				if (craftPlayer.hasPermission("bewom.admin") || craftPlayer.hasPermission("bewom.mod")){
-					List<String> pList = new ArrayList<String>();  
-			         
-					pList.add("privado");
-					pList.add("publico");
-					
-					return pList;
-				}
-			}
+		} 
 		
-		}
 		return null;
 	}
 
