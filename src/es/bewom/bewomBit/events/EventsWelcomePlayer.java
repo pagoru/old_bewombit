c5d5f0b272bec5791e3045c1ab45ee4543d91f08
Pablo <pagoru@gmail.com>
2014-08-21 06:46:26 +0200

    ++ Evento y message al desconectar

--- src/es/bewom/bewomBit/events/EventsWelcomePlayer.java
+++ src/es/bewom/bewomBit/events/EventsWelcomePlayer.java
@@ -2,6 +2,7 @@ package es.bewom.bewomBit.events;
 
 import org.bukkit.ChatColor;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class EventsWelcomePlayer {
 
@@ -10,6 +11,14 @@ public class EventsWelcomePlayer {
 		String playerName = eventConnect.getPlayer().getName(); //limpio String 
 		
 		eventConnect.setJoinMessage(ChatColor.GRAY + playerName + ChatColor.GRAY + " ha entrado en el servidor.");	
+
+	}
+	
+	public static void connectPlayerEventsGoodbyePlayer (PlayerQuitEvent eventQuit){
+
+		String playerName = eventQuit.getPlayer().getName(); //limpio String 
+		
+		eventQuit.setQuitMessage(ChatColor.GRAY + playerName + ChatColor.GRAY + " ha salido del servidor.");	
 
 	}
 }
